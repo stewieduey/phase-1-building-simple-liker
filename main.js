f9f8313e493173d1dccc265327aa0e10e0f52e63
DuMark Allman <dumark.allman@yahoo.com>
2023-04-15 23:59:04 -0400

    [stewieduey]

--- main.js
+++ main.js
@@ -2,10 +2,69 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// Your JavaScript code goes here!!
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOMContentLoaded!")
+//add .hidden
+const errorModal = document.querySelector("#modal")
+errorModal.classList.add("hidden")
+
+
+//findLikes()
+clickListener()
+});
+
+function hideError(){
+  errorModal.classList.add("hidden")
+}
 
 
 
+
+function findLikes(){
+const likeArr =  document.querySelectorAll(".like-glyph)")
+likeArr.forEach((singularLike) => {
+  singularLike.addEventListener('click', () => console.log("LIKE!"))
+})
+}
+
+function clickListener(){
+  document.addEventListener('click', (event) => {
+
+
+   if (event.target.classList[0] === 'like-glyph'){
+    // PROMISE --.then???
+
+    mimicServerCall()
+    .then(resp => {
+      const activated = event.target.classList.contains("activated-heart");
+        if(activated) {
+        event.target.classList.remove("activated-heart")
+        event.target.innerHTML = EMPTY_HEART;
+        }else {
+          event.target.classList.add("activated-heart")
+          event.target.innerHTML = FULL_HEART;
+        }
+          activated 
+           
+          
+    })
+      
+    .catch(error => {
+      setTimeout(() => {
+        hideError()
+        }, 3000);
+      })
+      
+      
+      // PROMISE FAIL
+    }
+  
+  })
+
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
